a14ce3a945aa58ac3288918cac380ed6de97fbe5
LFroesch <lucas.froeschner@gmail.com>
2024-09-10 14:28:18 -0700

    fresh

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+
+    def __init__(self, width, height):
+        self.__root = Tk() #designate root with a new
+        self.__root.title = "Maze Solver" #use .title 'tkinter' method to set title on self.root
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
