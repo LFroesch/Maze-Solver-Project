045321729055f9c3bdaf78eb607211324ff7242d
LFroesch <lucas.froeschner@gmail.com>
2024-09-11 11:45:56 -0700

    working maze paths

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from maze import Maze
 
 
 def main():
-    num_rows = 12
-    num_cols = 16
+    num_rows = 8
+    num_cols = 8
     margin = 50
     screen_x = 800
     screen_y = 600
@@ -15,7 +15,7 @@ def main():
     print(f"Launching {title}")
     print("Placeholder CLI Instructions")
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
 
     win.wait_for_close()
 
